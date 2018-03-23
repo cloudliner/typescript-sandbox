0dff98e48b851c212cb94f4f9e2b5335dc7c33b9
cloudliner <cloudliner.jp@gmail.com>
2018-03-24 06:55:14 +0900

    List 2-1

---
+++ src/list-2/2-1.ts
@@ -0,0 +1,18 @@
+var dynamic = 'A string';
+
+dynamic = 52;
+
+var days = 7;
+var hours = '24';
+
+var week = days * hours;
+
+var twodays = hours + hours;
+
+function getVolume(width, height, depth) {
+  return width * height * depth;
+}
+
+var volumeA = getVolume(10);
+
+var volumeB = getVolume(2, 4, 4, 8);
