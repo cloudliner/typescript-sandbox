7ce03303af632d8e873e5d53d69c0065e32bf5e6
cloudliner <cloudliner.jp@gmail.com>
2018-03-28 02:21:07 +0900

    List 6-2

---
+++ src/list-6/6-2.ts
@@ -0,0 +1,15 @@
+import http = require("http");
+
+const portNumber = "8080";
+
+function requestListener(request: http.ServerRequest, response: http.ServerResponse) {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.write(`Method: ${ request.method }\n`);
+  response.write(`URL: ${ request.url }\n`)
+  response.write("Response Text Here");
+  response.end();
+}
+
+http.createServer(requestListener).listen(portNumber);
+
+console.log(`Listening on localhost:${ portNumber }`);
