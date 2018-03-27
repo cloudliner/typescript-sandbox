c0be0d5066535bead6b7fc82adbf4fc53cf1f090
cloudliner <cloudliner.jp@gmail.com>
2018-03-28 01:11:16 +0900

    List 5-4

---
+++ src/list-5/5-4.ts
@@ -0,0 +1,8 @@
+var a = document.getElementById('content');
+console.log(a);
+
+var b = document.querySelector('#content');
+console.log(b);
+
+var c = <HTMLDivElement> document.querySelector('#content');
+console.log(c);
